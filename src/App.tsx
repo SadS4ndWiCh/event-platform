cfc481b914de795413f1dd36f6d86a518d30fb17
SadS4ndWiCh <caiocamargo007@gmail.com>
2022-06-25 21:21:39 -0300

    add skeleton theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { Router } from "./Router"
 
 import { client } from './lib/apollo';
 import { BrowserRouter } from "react-router-dom";
+import { SkeletonTheme } from "react-loading-skeleton";
 
 function App() {
   return (
     <ApolloProvider client={client}>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <SkeletonTheme baseColor='#202020' highlightColor='#444'>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </SkeletonTheme>
     </ApolloProvider>    
   )
 }
